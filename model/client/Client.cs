c946a8ece80d55f2ab33c6caebc3e9685f51956a
Aleksander Januszewski <alex.januszewski@interia.pl>
2019-08-12 19:27:17 +0200

    Changed name of Kontrahent.cs to Client.cs and added INotifyPropertyChanged and fit fields to it

---
+++ model/client/Client.cs
@@ -0,0 +1,93 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Fiscal_Management_System.model.client
+{
+    [Table("Kontrahent")]
+    public class Client : INotifyPropertyChanged
+    {
+        [Column("kontrahent_id")]
+        public int ID { get; set; }
+
+        private string _name;
+        [Column("kontrahent_nazwa")]
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; OnPropertyChanged("Name"); }
+        }
+
+        private string _symbol;
+        [Column("kontrahent_symbol")]
+        public string Symbol
+        {
+            get { return _symbol; }
+            set { _symbol = value;  OnPropertyChanged("Symbol"); }
+        }
+
+        private string _nip;
+        [Column("kontrahent_nip")]
+        public string NIP
+        {
+            get { return _nip; }
+            set { _nip = value; OnPropertyChanged("NIP"); }
+        }
+
+        private string _state;
+        [Column("kontrahent_wojewodztwo")]
+        public string State
+        {
+            get { return _state; }
+            set { _state = value; OnPropertyChanged("State"); }
+        }
+
+        private string _city;
+        [Column("kontrahent_miasto")]
+        public string City
+        {
+            get { return _city; }
+            set { _city = value; OnPropertyChanged("City"); }
+        }
+
+        private string _street;
+        [Column("kontrahent_ulica")]
+        public string Street
+        {
+            get { return Street; }
+            set { _street = value; OnPropertyChanged("Street"); }
+        }
+
+        private string _postalCode;
+        [Column("kontrahent_kod_pocztowy")]
+        public string PostalCode
+        {
+            get { return _postalCode; }
+            set { _postalCode = value; OnPropertyChanged("PostalCode"); }
+        }
+
+        private string _phone;
+        [Column("kontrahent_telefon")]
+        public string Phone
+        {
+            get { return _phone; }
+            set { _phone = value; OnPropertyChanged("Phone"); }
+        }
+
+        private string _email;
+        [Column("kontrahent_email")]
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; OnPropertyChanged("Email"); }
+        }
+
+        #region INotifyPropertyChanged things
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
