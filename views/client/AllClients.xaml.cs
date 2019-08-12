73f6cfaec5cddfc13c12fd0c7e7e2c3f946a1c8c
Aleksander Januszewski <alex.januszewski@interia.pl>
2019-08-12 17:52:37 +0200

    Added AllClients view

---
+++ views/client/AllClients.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Fiscal_Management_System.views.client
+{
+    /// <summary>
+    /// Interaction logic for AllClients.xaml
+    /// </summary>
+    public partial class AllClients : UserControl
+    {
+        public AllClients()
+        {
+            InitializeComponent();
+        }
+    }
+}
