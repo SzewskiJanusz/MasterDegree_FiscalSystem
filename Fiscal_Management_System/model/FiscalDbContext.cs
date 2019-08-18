7824c70eee4f2a99ee534f5a795297932ca7ee29
Aleksander Januszewski <alex.januszewski@interia.pl>
2019-08-18 12:44:40 +0200

    Added EntityFramework context class

---
+++ Fiscal_Management_System/model/FiscalDbContext.cs
@@ -0,0 +1,38 @@
+namespace Fiscal_Management_System.model
+{
+    using Fiscal_Management_System.model.client;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.ModelConfiguration.Conventions;
+    using System.Linq;
+
+    public class FiscalDbContext : DbContext
+    {
+        // Your context has been configured to use a 'Model' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'Fiscal_Management_System.model.Model' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'Model' 
+        // connection string in the application configuration file.
+        public FiscalDbContext()
+            : base("name=FiscalDbContext")
+        {
+        }
+        public DbSet<Client> Clients { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
