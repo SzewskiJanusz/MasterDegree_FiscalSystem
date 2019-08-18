a7402e3ee5798b725e0d2f669e2295f047d194a8
Aleksander Januszewski <alex.januszewski@interia.pl>
2019-08-18 12:43:52 +0200

    Added contructor in code-behind of AddOrEditClient.xaml to enable editing window

--- Fiscal_Management_System/views/client/AddOrEditClient.xaml.cs
+++ Fiscal_Management_System/views/client/AddOrEditClient.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Fiscal_Management_System.viewmodels.client;
+﻿using Fiscal_Management_System.model.client;
+using Fiscal_Management_System.viewmodels.client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,12 @@ namespace Fiscal_Management_System.views.client
         {
             InitializeComponent();
             this.DataContext = new ClientViewModel();
+        }
+
+        public AddOrEditClient(Client c)
+        {
+            InitializeComponent();
+            this.DataContext = new ClientViewModel(c);
         }
     }
 }
