210fa24f5f4f8cabfff083e394b0c4f1e43cd234
Aleksander Januszewski <alex.januszewski@interia.pl>
2019-08-18 12:40:15 +0200

    Added WindowTitle and ButtonText properties and used copy contructor of client

--- Fiscal_Management_System/viewmodels/client/ClientViewModel.cs
+++ Fiscal_Management_System/viewmodels/client/ClientViewModel.cs
@@ -8,20 +8,19 @@ namespace Fiscal_Management_System.viewmodels.client
 {
     public class ClientViewModel
     {
+        /// <summary>
+        /// Provides searching mechanisms
+        /// </summary>
         private EntitySearcher<Client> _entitySearcher;
         public EntitySearcher<Client> EntitySearcher
         {
-            get
-            {
-                return _entitySearcher;
-            }
-
-            set
-            {
-                _entitySearcher = value;
-            }
+            get { return _entitySearcher; }
+            set { _entitySearcher = value; }
         }
 
+        /// <summary>
+        /// Binding client with datagrid and textboxes during addition
+        /// </summary>
         private Client _client;
         public Client Client
         {
@@ -29,11 +28,17 @@ namespace Fiscal_Management_System.viewmodels.client
             set { _client = value; }
         }
 
+        /// <summary>
+        /// Collection used to view data in datagrid
+        /// </summary>
         public ObservableCollection<Client> Clients
         {
             get;
             set;
         }
+
+        public string ButtonText { get; set; }
+        public string WindowTitle { get; set; }
 
         private ICommand _confirmAdditionOfClientButtonCommand;
         public ICommand ConfirmAdditionOfClientButtonCommand
@@ -45,8 +50,15 @@ namespace Fiscal_Management_System.viewmodels.client
                     _confirmAdditionOfClientButtonCommand = new RelayCommand(o =>
                     {
                         ClientService cs = new ClientService();
-                        cs.Add((Client)o);
-
+                        Client unboundClient = new Client((Client)o);
+                        if (ButtonText == "OK")
+                        {
+                            cs.Edit(unboundClient);
+                        }
+                        else
+                        {
+                            cs.Add(unboundClient);
+                        }
                     }, o => true);
                 }
 
@@ -69,13 +81,40 @@ namespace Fiscal_Management_System.viewmodels.client
             }
         }
 
+        private ICommand _goToEditClientButtonCommand;
+        public ICommand GoToEditClientButtonCommand
+        {
+            get
+            {
+                _goToEditClientButtonCommand = new RelayCommand(o =>
+                {
+                    Client unboundClient = new Client((Client)o);
+                    AddOrEditClient editClientWindow = new AddOrEditClient(unboundClient);
+                    editClientWindow.Show();
+                }, o => true);
+
+                return _goToEditClientButtonCommand;
+            }
+        }
+
         public ClientViewModel()
         {
             ClientService cs = new ClientService();
             Clients = cs.GetAll();
             EntitySearcher = new EntitySearcher<Client>(Clients);
             Client = new Client() { Name = "Client's name" };
+            ButtonText = "Dodaj";
+            WindowTitle = "Dodawanie kontrahenta";
         }
 
+        public ClientViewModel(Client c)
+        {
+            ClientService cs = new ClientService();
+            Clients = cs.GetAll();
+            EntitySearcher = new EntitySearcher<Client>(Clients);
+            Client = new Client(c);
+            ButtonText = "OK";
+            WindowTitle = "Edytowanie kontrahenta";
+        }
     }
 }
