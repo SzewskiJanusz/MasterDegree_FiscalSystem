8478a252d99c808fd1a88e2354ce8bb83d273dcb
Aleksander Januszewski <alex.januszewski@interia.pl>
2020-01-20 08:40:56 +0100

    Added menuitem style triggers in MainWindow

--- Fiscal_Management_System/views/MainWindow.xaml.cs
+++ Fiscal_Management_System/views/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using Fiscal_Management_System.viewmodels;
+﻿using Fiscal_Management_System.model;
+using Fiscal_Management_System.viewmodels;
+using System;
+using System.Threading;
 using System.Windows;
 using System.Windows.Markup;
+using System.Windows.Threading;
 
 namespace Fiscal_Management_System.views
 {
@@ -9,11 +13,27 @@ namespace Fiscal_Management_System.views
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        Window loginWindow;
+
+        public MainWindow(Serviceman user, Window w)
         {
             InitializeComponent();
+            loginWindow = w;
             this.Language = XmlLanguage.GetLanguage("pl-PL");
-            this.DataContext = new MainViewModel();
+            this.Closed += CloseLoginWindow;
+            this.DataContext = new MainViewModel(user);
+        }
+
+        private void CloseLoginWindow(object sender, System.EventArgs e)
+        {
+            try
+            {
+                if (loginWindow.Dispatcher.CheckAccess())
+                    loginWindow.Close();
+                else
+                    loginWindow.Dispatcher.Invoke(DispatcherPriority.Normal, new ThreadStart(loginWindow.Close));
+            }
+            catch (Exception) { }
         }
     }
 }
