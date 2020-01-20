19a0b24cae58732d9e99c759dd99acb863f51f4e
Aleksander Januszewski <alex.januszewski@interia.pl>
2020-01-20 08:34:09 +0100

    Added RepairGroup and Serviceman model

---
+++ Fiscal_Management_System/model/repairgroup/RepairGroup.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Fiscal_Management_System.model.device;
+
+namespace Fiscal_Management_System.model.repairgroup
+{
+    [Table("GrupaNaprawcza")]
+    public class RepairGroup
+    {
+        [Column("grupa_id")]
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Name and surname of serviceman
+        /// </summary>
+        [Column("grupa_ktory_pierwszy")]
+        public string FirstServiceman { get; set; }
+
+        [Column("urzadzenie_id")]
+        public int DeviceId { get; set; }
+        [ForeignKey("DeviceId")]
+        public Device Device { get; set; }
+
+        [Column("serwisant_id")]
+        public int ServicemanId { get; set; }
+        [ForeignKey("ServicemanId")]
+        public Serviceman Serviceman { get; set; }
+    }
+}
---
+++ Fiscal_Management_System/model/serviceman/Serviceman.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using Fiscal_Management_System.model.repairgroup;
+
+namespace Fiscal_Management_System.model
+{
+    [Table("Serwisant")]
+    public class Serviceman
+    {
+        [Column("serwisant_id")]
+        public int ID { get; set; }
+
+        [Column("serwisant_imie")]
+        public string Name { get; set; }
+
+        [Column("serwisant_nazwisko")]
+        public string Surname { get; set; }
+
+        [Column("serwisant_hash")]
+        public string Hash { get; set; }
+
+        [Column("serwisant_salt")]
+        public string Salt { get; set; }
+
+        [Column("serwisant_telefon")]
+        public string Phone { get; set; }
+
+        [Column("serwisant_email")]
+        public string Email { get; set; }
+
+        [Column("serwisant_czyglowny")]
+        public bool IsMain { get; set; }
+
+        [NotMapped]
+        public string NameAndSurname { get { return Name + " " + Surname; } }
+
+        // 0:n on RepairGroup relation
+        public ICollection<RepairGroup> Groups { get; set; }
+    }
+}
