04699ac88dc6eba08e9fe59a646515143d40dd72
Aleksander Januszewski <alex.januszewski@interia.pl>
2020-01-20 08:38:29 +0100

    Added viewmodel for set service as done

--- Fiscal_Management_System/viewmodels/service/SetServiceAsDoneViewModel.cs
+++ Fiscal_Management_System/viewmodels/service/SetServiceAsDoneViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Runtime.Remoting.Contexts;
 using System.Text;
@@ -7,6 +8,7 @@ using System.Windows;
 using System.Windows.Input;
 using Fiscal_Management_System.model;
 using Fiscal_Management_System.model.client;
+using Fiscal_Management_System.model.device;
 using Fiscal_Management_System.model.place;
 using Fiscal_Management_System.model.service;
 using Fiscal_Management_System.views.device;
@@ -46,11 +48,25 @@ namespace Fiscal_Management_System.viewmodels.service
 
         public void AddDateOfExecutionInService(Service service)
         {
+            Service s;
+            Service entityInDb;
+            DateTime next;
+            Device d;
             using (var ctx = Context == null ? new FiscalDbContext() : Context)
             {
-                var entityInDb = ctx.Set<Service>().Find(service.Id);
+                entityInDb = ctx.Set<Service>().Include("Device").Include("Device.Client").Include("Device.Place").FirstOrDefault(x => x.Id == service.Id);
                 entityInDb.ExecutionTime = service.PlannedDateOfExecution;
                 entityInDb.ChargedPrice = service.ChargedPrice;
+                next = entityInDb.ExecutionTime.Value.
+                    AddMonths(entityInDb.Device.InspectionPeriodicTimeInMonths);
+                d = new Device(entityInDb.Device);
+                s = new Service(service);
+                s.Device = ctx.Set<Device>().FirstOrDefault(x => x.UniqueNumber == d.UniqueNumber);
+                s.TypeOfService = ctx.Set<TypeOfService>().FirstOrDefault(x => x.Name == "Przegląd kasy fiskalnej");
+                s.ApproveTime = (DateTime)entityInDb.ExecutionTime;
+                s.ExecutionTime = null;
+                s.PlannedDateOfExecution = next;
+                ctx.Set<Service>().Add(s);
                 ctx.SaveChanges();
             }
         }
