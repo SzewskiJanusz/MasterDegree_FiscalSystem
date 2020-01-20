d572ecbf9b8b9a4af17fb61435ca864db70abe77
Aleksander Januszewski <alex.januszewski@interia.pl>
2020-01-20 08:39:46 +0100

    Added assign servicemen view

---
+++ Fiscal_Management_System/views/device/AssignServicemen.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using Fiscal_Management_System.viewmodels.device;
+
+namespace Fiscal_Management_System.views.device
+{
+    /// <summary>
+    /// Interaction logic for AssignServicemen.xaml
+    /// </summary>
+    public partial class AssignServicemen : Window
+    {
+        public AssignServicemen()
+        {
+            InitializeComponent();
+            this.DataContext = new AssignServicemenViewModel();
+        }
+
+        public void closeWindow(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+    }
+}
